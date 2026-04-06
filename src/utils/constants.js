891f70318a7a7e6e6f74d4eedea9ce931b7da2a1
mangeshlawande1 <mangeshlawande511@gmail.com>
2026-04-06 16:14:08 +0530

    write constants

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+export const UserRoleEnum =  {
+    ADMIN:"admin",
+    PROJECT_ADMIN:"project_admin",
+    MEMBER:"member"
+};
+
+//array
+export const AvailableUserRole = Object.values(UserRoleEnum)
+
+//object
+export const TaskStatusEnum = {
+    TODO:"todo",
+    IN_PROGRESS : "in_progress",
+    DONE:"done",
+}
+// array
+export const AvailableTaskStatues = Object.values(TaskStatusEnum)
+
+    
