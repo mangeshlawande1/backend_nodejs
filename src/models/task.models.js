3013c10cbde800e7498e867194921d8bef2588c6
mangeshlawande1 <mangeshlawande511@gmail.com>
2026-04-09 14:19:28 +0530

    write models

---
+++ src/models/task.models.js
@@ -0,0 +1,45 @@
+import { AvailableTaskStatues, TaskStatusEnum } from "#utils/constants.js";
+import mongoose, { Schema } from "mongoose";
+
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: String,
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+    assignedTo: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    assignedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    status: {
+        type: String,
+        enum: AvailableTaskStatues,
+        default: TaskStatusEnum.TODO,
+
+    },
+    attachments: {
+        type: [{
+            url: String,
+            mimeType: String,
+            size: Number,
+        }],
+        default: [],
+    }
+
+}, { timestamps: true });
+
+
+
+export const Task = mongoose.model("Task", taskSchema);
+
