29536c85d11450813ab2272732ec68b89e6f12df
mangeshlawande1 <mangeshlawande511@gmail.com>
2026-04-13 14:24:25 +0530

    add validProjectPermission middlware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,9 @@
+import { ProjectMember } from "#models/projectmember.models.js";
 import { User } from "#models/user.models.js";
 import { ApiError } from "#utils/ApiError.js";
 import { asyncHandler } from "#utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
@@ -32,3 +34,32 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
 
 
 });
+
+export const validateProjectPermission = (roles = []) => {
+    asyncHandler(async (req, res, next) => {
+        const { projectId } = req.params
+        if (!projectId) {
+            throw new ApiError(400, "Project Id not Found !!")
+        }
+
+        let project = await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user: new mongoose.Types.ObjectId(req.user._id),
+        });
+
+        if (!project) {
+            throw new ApiError(400, "Project  not Found !!")
+        }
+        const givenRole = project?.role;
+
+        req.user.role = givenRole
+
+        if (!roles.includes(givenRole)) {
+            throw new ApiError(
+                403, "You do not have Permission to perform this action !"
+            )
+        }
+        next();
+    });
+
+};
