b0f5a732514900f2e2c2e6780351f97767139059
mangeshlawande1 <mangeshlawande511@gmail.com>
2026-05-04 16:12:02 +0530

    ApiError

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,15 +1,15 @@
 class ApiError extends Error {
   constructor(
     statusCode,
-    message = 'Something went Wrong !!',
+    message = "Something went wrong",
     errors = [],
-    stack = '',
+    stack = ""
   ) {
     super(message);
+
     this.statusCode = statusCode;
-    this.data = null;
-    this.message = message;
     this.success = false;
+    this.message = message;
     this.errors = errors;
 
     if (stack) {
