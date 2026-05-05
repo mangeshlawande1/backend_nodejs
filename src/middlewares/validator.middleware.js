db73494d3a31aaed97a6a591d351baf8cd70d9fb
mangeshlawande1 <mangeshlawande511@gmail.com>
2026-05-05 15:16:48 +0530

    update validator

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,14 +1,31 @@
-import { ApiError } from '#utils/ApiError.js';
-import { validationResult } from 'express-validator';
+import { validationResult } from "express-validator";
+
+import { ApiError } from "#utils/ApiError.js";
 
 export const validate = (req, _, next) => {
   const errors = validationResult(req);
 
-  if (errors.isEmpty()) return next();
+  if (errors.isEmpty()) {
+    return next();
+  }
 
-  const extractedErrors = errors.array().map((err) => ({
-    [err.path]: err.msg,
-  }));
+  const extractedErrors = {};
 
-  throw new ApiError(400, 'Invalid request data', extractedErrors);
+  errors.array().forEach((err) => {
+    const field = err.path || err.param;
+
+    if (!extractedErrors[field]) {
+      extractedErrors[field] = [];
+    }
+
+    extractedErrors[field].push(err.msg);
+  });
+
+  return next(
+    new ApiError(
+      400,
+      "Validation failed",
+      extractedErrors
+    )
+  );
 };
