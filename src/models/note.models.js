22c1ed3d2e71b4b211b778f2bd354d294056311e
mangeshlawande1 <mangeshlawande511@gmail.com>
2026-05-06 10:52:19 +0530

    project indexing

--- src/models/note.models.js
+++ src/models/note.models.js
@@ -1,23 +1,72 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 const projectNoteSchema = new Schema(
   {
     project: {
       type: Schema.Types.ObjectId,
-      ref: 'Project',
+      ref: "Project",
       required: true,
+      index: true,
     },
+
     createdBy: {
       type: Schema.Types.ObjectId,
-      ref: 'User',
+      ref: "User",
       required: true,
+      index: true,
     },
+
     content: {
       type: String,
-      required: true,
+      required: [true, "Note content is required"],
+      trim: true,
+      minlength: 1,
+      maxlength: 5000,
+    },
+
+    isPinned: {
+      type: Boolean,
+      default: false,
+      index: true,
+    },
+
+    editedAt: {
+      type: Date,
+      default: null,
     },
   },
-  { timestamps: true },
+
+  {
+    timestamps: true,
+  }
 );
 
-export const ProjectNote = mongoose.model('ProjectNote', projectNoteSchema);
+/* =========================================================
+   INDEXES
+========================================================= */
+
+projectNoteSchema.index({ project: 1, createdAt: -1, });
+
+projectNoteSchema.index({
+  project: 1,
+  isPinned: 1,
+});
+
+/* =========================================================
+   AUTO UPDATE editedAt
+========================================================= */
+
+projectNoteSchema.pre(
+  "save",
+  function (next) {
+    if (
+      this.isModified("content") &&
+      !this.isNew
+    ) {
+      this.editedAt = new Date();
+    }
+    next();
+  }
+);
+
+export const ProjectNote = mongoose.model("ProjectNote", projectNoteSchema);
