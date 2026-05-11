311ba4297337ffbcea97fb1ebfdd655a114d7c47
mangeshlawande1 <mangeshlawande511@gmail.com>
2026-05-11 14:31:41 +0530

    fix: fix imports

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,12 +1,10 @@
-import { User } from "../models/user.model.js";
+import { User } from "#models/user.models.js";
 
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "#utils/ApiError.js";
+import { ApiResponse } from "#utils/ApiResponse.js";
+import { asyncHandler } from "#utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
-
 import {
   emailVerificationMailgenContent,
   forgotPasswordMailgenContent,
